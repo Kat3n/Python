be1d510a6dce8313045a98bec57b66fb863b1673
unknown <paliakova1987@mail.ru>
2022-04-06 13:21:29 +0300

    variables, dataTypes

---
+++ course_1.py
@@ -0,0 +1,42 @@
+lesson 1
+
+# while True:
+#     name = input()
+#     print("Your input word = ", name)
+
+# вывод тип данных
+# a = 2
+# b = 5.5
+# print("type a = ", type(a))
+# print("type b = ", type(b))
+# print(a+b, type(a+b))
+
+# #вывод списка массива
+# a, b, c = 5, 8, 0
+# name_list = [a, b, c]
+# for i in name_list:
+#     print("nomber = ", i)
+
+# param_1 = 7
+# param_2 = 2
+# param_3 = param_1/param_2
+# param_4 = param_1//param_2         #целочисленное деление
+# param_5 = param_1 % param_2        #остаток от деления
+# param_6 = 3+4*5**2+7               #приоритетность выполнения
+# print('param_3 =',param_3, type(param_3))
+# print('param_4 =', param_4, type(param_4))
+# print('param_5 =', param_5, type(param_5))
+# print('param_6 =', param_6, type(param_6))
+
+# #операции с присвоением
+# param_1 = 7
+# param_1 += 2
+# print('param_1 =',param_1, type(param_1))
+
+# #подсчет элементов в массиве
+# name_list = ['hkjhj', 'hggg', 'hgjhgjh', 'sggrd', 'rgsga', 'grrgrrh']
+# count = 0
+# for i in name_list:
+#     print('count = ', count, '--i', i, type(i))
+#     count += 1
+
